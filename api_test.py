eee4d6a89be63721eda8f1f547db674f5d9a6214
kevindenny <kevin.denny@piano.io>
2017-10-27 14:30:09 -0400

    Updated query test to modularly load from es_functions

--- api_test.py
+++ api_test.py
@@ -1,104 +1,48 @@
 import requests
 import json
 from pprint import pprint
+from es_functions import *
 
+new_dataset = {
+        'id': 5,
+        'name': 'More Random Data',
+        'url': 'http://nimbuscharts.pythonanywhere.com/sheets/13eU1yFNUO6guspeQRFRiVPugp0Ihrjym8BSXnKL6Ukw/Sheet1/',
+        'fields': ['DMA Name', 'Conversions'],
+        'publisher_id': 'o19WonOrHQ'
+    }
 
-def add_object(data):
-    url = 'http://localhost:9200/reports/report/'
-
-    headers = {'content-type': 'application/json'}
-
-    r = requests.post(url, data=json.dumps(data), headers=headers)
-
-    return r.text
-
-def get_objects():
-    url = 'http://localhost:9200/reports/_search'
-
-    headers = {'content-type': 'application/json'}
-
-    r = requests.get(url, headers=headers)
-
-    return json.loads(r.text)
-
-
-
-
-def search_query(qstring, fields):
-    if qstring != '':
-        search_obj = {
-            "query": {
-                "query_string": {
-                    "query": qstring,
-                    "fields": fields
-                }
-            }
+new_chart = {
+    'id': 'XTy34v',
+    'type': 'line',
+    'data': new_dataset,
+    'render_schema': {
+        'xVar': 'DMA Name',
+        'yVar': 'Conversions',
+        'xLabel': 'DMA Name',
+        'yLabel': 'Conversions',
+    },
+    'properties': {
+        'type': {
+            "type": "text",
+            "fielddata": True
+        },
+        'data': {
+            "type": "nested"
+        },
+        'renderSchema': {
+            "type": "nested"
         }
-    else:
-        search_obj = {
-            "_source": ["chart.data.id", "chart.data.url"],
-            # "query": {
-            #     "exists" : { "field" : "chart.data" }
-            # },
-            "size": 0,
-            "aggs" : {
-                "langs" : {
-                    "terms" : { "field" : "chart.data.id"}
-                }
-            }
-        }
+    }
+}
 
-    url = 'http://localhost:9200/_search'
-
-    headers = {'content-type': 'application/json'}
-
-    r = requests.post(url, data=json.dumps(search_obj), headers=headers)
-
-    pprint(json.loads(r.text))
-
-def get_index_length(ind_name):
-    url = 'http://localhost:9200/{0}/_search'.format(ind_name)
-
-    headers = {'content-type': 'application/json'}
-
-    r = requests.get(url, headers=headers)
-
-    all_res = json.loads(r.text)['hits']['total']
-    print(all_res)
-
-
-
-
-
-
-
-
-
-
-
-reports_list = [{
+new_report = {
     "title": "Pageviews by DMA",
     "description": "Bar Chart for a small subset of DMAs",
     "layout": {'row_1': {'id': 1,
         'width': 900,
         'height': 550}
     },
-    "chart": [{
-        'type': 'line',
-        'data': {
-            'id': 2,
-            'name': 'Publisher Benchmarking',
-            'url': 'http://nimbuscharts.pythonanywhere.com/sheets/13eU1yFNUO6guspeQRFRiVPugp0Ihrjym8BSXnKL6Ukw/Sheet1/',
-            'fields': ['DMA Name', 'Conversions'],
-            'publisher_id': 'o19WonOrHQ'
-        },
-        'render_schema': {
-            'xVar': 'DMA Name',
-            'yVar': 'Conversions',
-            'xLabel': 'DMA Name',
-            'yLabel': 'Conversions',
-        }
-    }],
+    "chart": [new_chart],
     "properties":{
       "title" : {
         "type" : "string"
@@ -113,29 +57,20 @@ reports_list = [{
         "type" : "nested"
       }
     }
-    
-}]
+}
 
-# for report in reports_list:
+# bt = create_report(new_report)
+# print(bt)
 
-#     b = json.loads(add_object(report))
-#     pprint(b)
-
-# a = get_objects()
-# pprint(a['hits'])
-
-# get_index_length('reports')
-
-# query = '2'
-# # query = ''
-# fields = ['chart.data.id']
-
-# search_query(query, fields)
+# a = json.loads(get_all_datasets())
 
 
-a = {'df': 12}
-from elasticsearch_api import Reports
+# charts = json.loads(get_all_charts())
+# pprint(charts)
 
-report_api = Reports()
+# b = update_mapping('report','chart.type')
+# pprint(b)
 
-report_api.create(data=a)
+a = get_all_reports()
+pprint(json.loads(a))
+
